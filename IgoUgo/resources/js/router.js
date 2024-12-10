e44524fe414eac512892bdd99a99046b8e82471b
jji-min <6wnla9@naver.com>
2024-12-10 10:03:09 +0900

    241210 김지민 Component 추가, router.js 수정

--- IgoUgo/resources/js/router.js
+++ IgoUgo/resources/js/router.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HotelListComponent from '../views/components/board/HotelListComponent.vue';
+import LoginComponent from '../views/components/auth/LoginComponent.vue';
+import UserRegistrationComponent from '../views/components/auth/UserRegistrationComponent.vue';
+import ProductListComponent from '../views/components/board/ProductListComponent.vue';
+import BoardListComponent from '../views/components/board/BoardListComponent.vue';
+import BoardCreateComponent from '../views/components/board/BoardCreateComponent.vue';
+import BoardUpdateComponent from '../views/components/board/BoardUpdateComponent.vue';
+import NotFoundComponent from '../views/NotFoundComponent.vue';
 
 const routes = [
   // 다른 경로와 컴포넌트 추가 가능
-  {
-    path: '/',
-    redirect: 'login',
-  },
   {
     path: '/login',
     component: LoginComponent,
