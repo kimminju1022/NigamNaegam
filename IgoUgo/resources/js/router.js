4812dd16ee74abda278d0bd77d7f47783e924b01
jji-min <6wnla9@naver.com>
2024-12-10 09:49:37 +0900

    241210 김지민 router.js 수정

--- IgoUgo/resources/js/router.js
+++ IgoUgo/resources/js/router.js
@@ -2,11 +2,43 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HotelListComponent from '../views/components/board/HotelListComponent.vue';
 
 const routes = [
+  // 다른 경로와 컴포넌트 추가 가능
+  {
+    path: '/',
+    redirect: 'login',
+  },
+  {
+    path: '/login',
+    component: LoginComponent,
+  },
+  {
+    path: '/registration',
+    component: UserRegistrationComponent,
+  },
   {
     path: '/hotels',
     component: HotelListComponent,
   },
-  // 다른 경로와 컴포넌트 추가 가능
+  {
+    path: '/products',
+    component: ProductListComponent,
+  },
+  {
+    path: '/boards',
+    component: BoardListComponent,
+  },
+  {
+    path: '/boards/create',
+    component: BoardCreateComponent,
+  },
+  {
+    path: '/boards/update',
+    component: BoardUpdateComponent,
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: NotFoundComponent,
+  },
 ];
 
 
