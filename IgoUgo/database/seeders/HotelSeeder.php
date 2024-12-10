20d65b27ce004ff50ce34d6897a2362cfff8e0cf
YJS2000 <yjs03744959@gmail.com>
2024-12-10 18:18:29 +0900

    241210 윤종승 호텔리스트컴포넌트

--- IgoUgo/database/seeders/HotelSeeder.php
+++ IgoUgo/database/seeders/HotelSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Hotel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,39 @@ class HotelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $hotel = [
+            [
+                'hc_id' => 1,
+                'hotel_latitude' => 35.866910,
+                'hotel_longitude' => 128.594506,
+                'hotel_title' => '토요코인호텔',
+                'hotel_content' => '토요코인 대구 동성로에서 일본식 서비스와 현대적인 편안함을 경험하세요. 정원과 공용 라운지가 있는 조용한 환경을 즐기십시오. 무료 Wi-Fi와 24시간 프런트 데스크를 통해 항상 연결 상태를 유지할 수 있습니다. 대구 근처의 명소를 발견해보세요. 대구의 중심부에 위치한 토요코인 대구 동성로는 관광지, 쇼핑 지역 및 활기찬 나이트라이프에 쉽게 접근할 수 있습니다. 24시간 체크인, 매일 하우스키핑 및 아름다운 정원을 즐기십시오. 편안한 객실에는 에어컨, 무료 인터넷 접속 및 개인 금고가 갖추어져 있습니다. 서문시장, 대구 약령시 한방문화축제 및 대구 약령시 한의학 박물관을 탐험해보세요. 기억에 남는 숙박을 원하는 두 명의 여행객에게 완벽한 선택입니다.',
+                'hotel_address' => '대구 중구 동성로1길 15',
+                'hotel_price' => 71253
+            ],
+            [
+                'hc_id' => 1,
+                'hotel_latitude' => 35.867123,
+                'hotel_longitude' => 128.595940,
+                'hotel_title' => '호텔인섬니아',
+                'hotel_content' => '숙소는 동성로 중심가에 위치해 있어 접근성이 매우 좋으며, 주변에 다양한 식당과 놀거리가 풍부합니다. 객실은 깨끗하고 편안하며, 침대와 비품의 품질이 뛰어납니다. 고객들은 청결도와 시설에 대해 높은 만족도를 보였으며, 직원의 친절한 서비스도 긍정적으로 언급되었습니다. 조식이 제공되며, 전반적으로 쾌적한 분위기를 유지하고 있습니다.',
+                'hotel_address' => '대구 중구 동성로2길 27-1',
+                'hotel_price' => 75000
+            ],
+            [
+                'hc_id' => 1,
+                'hotel_latitude' => 35.867246,
+                'hotel_longitude' => 128.598104,
+                'hotel_title' => 'HOTEL AU',
+                'hotel_content' => '호텔 에이유에서의 숙박은 대구에서 탁월한 선택이라 할 수 있습니다. 이곳에서는 WiFi, 주차 대행, 셀프 주차 등의 무료 특전이 제공됩니다. 반월당역에서 도보로 9분, 중앙로역에서는 9분 거리에 있어 대중 교통편을 이용하기 편리합니다.',
+                'hotel_address' => '대구 중구 동성로2길 27-1',
+                'hotel_price' => 210000
+            ]
+        ];
+
+
+        foreach ($hotel as $hotelData) {
+            Hotel::create($hotelData); // id는 자동 생성됨
+        }
     }
 }
--- IgoUgo/resources/views/components/board/HotelListComponent.vue
+++ IgoUgo/resources/views/components/board/HotelListComponent.vue
@@ -1,50 +1,142 @@
 <template>
-    <div class="filter-price">
-        <p class="text-box">필터링</p>
-        <input class="input-box" id="nosmoke" type="checkbox">
-        <label for="nosmoke">금연</label>
+<div class="filter-price">
+    <p class="text-box">필터링</p>
+    <input class="input-box" id="nosmoke" type="checkbox">
+    <label for="nosmoke">금연</label>
 
-        <input class="input-box" id="wifi" type="checkbox">
-        <label for="wifi">무료 Wi-fi</label>
+    <input class="input-box" id="wifi" type="checkbox">
+    <label for="wifi">무료 Wi-fi</label>
 
-        <input class="input-box" id="morning" type="checkbox">
-        <label for="morning">조식</label>
+    <input class="input-box" id="morning" type="checkbox">
+    <label for="morning">조식</label>
 
-        <input class="input-box" id="parking" type="checkbox">
-        <label for="parking">주차</label>
-    </div>
+    <input class="input-box" id="parking" type="checkbox">
+    <label for="parking">주차</label>
+
+    <p class="text-box">가격</p>
+    <input class="input-box" id="nosmoke" type="checkbox">
+    <label for="nosmoke">10만원 이하</label>
+
+    <input class="input-box" id="wifi" type="checkbox">
+    <label for="wifi">10만원 - 15만원</label>
+
+    <input class="input-box" id="morning" type="checkbox">
+    <label for="morning">15만원 - 20만원</label>
+
+    <input class="input-box" id="parking" type="checkbox">
+    <label for="parking">20만원이상</label>
+</div>
+
+<!-- 지도 div -->
+<div id="map"></div>
 </template>
 
 <script setup>
+import { onMounted, reactive } from 'vue';
+import env from '../../../js/env';
 
+
+let map = reactive(null);
+
+onMounted(() => {
+    if (window.kakao && window.kakao.maps) {
+        loadKakaoMap();
+    } else {
+        loadKakaoMapScript();
+    }
+});
+
+// 카카오맵 스크립트 다운로드
+const loadKakaoMapScript = () => {
+    const script = document.createElement('script');
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${env.kakaoMapAppKey}&autoload=false&libraries=services`; // &autoload=false api를 로드한 후 맵을 그리는 함수가 실행되도록 구현
+    script.onload = () => window.kakao.maps.load(loadKakaoMap); // 스크립트 로드가 끝나면 지도를 실행될 준비가 되어 있다면 지도가 실행되도록 구현
+
+    document.head.appendChild(script); // html>head 안에 스크립트 소스를 추가
+}
+
+// 카카오맵 화면에 로드
+const loadKakaoMap = () => {
+    const container = document.getElementById("map"); 
+    const options = {
+        center: new window.kakao.maps.LatLng(35.879388797, 128.628366313), 
+        level: 3
+    };
+
+    map = new window.kakao.maps.Map(container, options);
+    console.log(map);
+    // loadMaker();
+}
+
+// 카카오맵 마커 생성
+const loadMaker = () => {
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new window.kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch('대구 중구 동성로1길 15', function(result, status) {
+
+        // console.log(result) // 주소 위도경도 콘솔로그
+        // 정상적으로 검색이 완료됐으면 
+        if (status === window.kakao.maps.services.Status.OK) {
+
+            var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new window.kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            var infowindow = new window.kakao.maps.InfoWindow({
+                content: '<div style="width:150px;text-align:center;padding:6px 0;">토요코인호텔 동성로점</div>'
+            });
+            infowindow.open(map, marker);
+
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        } 
+    });    
+}
 </script>
 
 <style scoped>
-    .filter-price {
-        width: 90vw;
-        height: 250px;
-        border: 2px solid #000000;
-        border-radius: 20px;
-        margin: 25px auto;
-        padding: 20px;
-        font-size: 25px;
-    }
-    .text-box {
-        width: 100px;
-        height: 50px;
-        background-color: #01083a;
-        text-align: center;
-        align-content: center;
-        color: #ffff;
-        border-radius: 10px;
-    }
-    .input-box {
-        margin: 20px 5px 20px 50px;
-        width: 20px;
-        height: 20px;
-        border: 1px solid #000000;
-        border-radius: 10px;
-        appearance: none;
+.filter-price {
+    width: 1000px;
+    height: 250px;
+    border: 2px solid #000000;
+    border-radius: 20px;
+    margin: 25px auto;
+    padding: 20px;
+    font-size: 25px;
+}
+.text-box {
+    width: 100px;
+    height: 50px;
+    background-color: #01083a;
+    text-align: center;
+    align-content: center;
+    color: #ffff;
+    border-radius: 10px;
+}
+.input-box {
+    margin: 20px 5px 20px 50px;
+    width: 25px;
+    height: 25px;
+    border: 1px solid #000000;
+    border-radius: 12.5px;
+    appearance: none;
+    vertical-align: middle;
 
-    }
+}
+.kakao-map {
+    width: 100vw;
+    height: 800px;
+}
+#map {
+    width: 100%;
+    height: 800px;
+    z-index: 1;  /* 헤더가 맵 위에 올 수 있도록 설정 */
+}
 </style>
