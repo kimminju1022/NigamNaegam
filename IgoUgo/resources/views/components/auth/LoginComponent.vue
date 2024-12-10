798853bb3199eb9259118c6e5d10c9d8d4053080
LeeGyeongjinny <yikj555@gmail.com>
2024-12-10 09:58:04 +0900

    241210 이경진 Component 생성

---
+++ IgoUgo/resources/views/components/auth/LoginComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="form-box">
+        <h1>로그인</h1>
+        <input type="text" placeholder="이메일">
+        <input type="password" placeholder="비밀번호">
+        <button>로그인</button>
+        <button>회원가입</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    
+</style>
