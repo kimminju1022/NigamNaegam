9dc91848c8729ee0130f0c7cb2afb6a69b944c16
LeeGyeongjinny <yikj555@gmail.com>
2024-12-11 14:48:10 +0900

    241211 이경진 appComponent css hover

--- IgoUgo/resources/views/components/AppComponent.vue
+++ IgoUgo/resources/views/components/AppComponent.vue
@@ -26,9 +26,19 @@
             </div>
         </div>
         <div class="header-list">
-            <button class="btn">호텔</button>
-            <button class="btn">상품</button>
-            <button class="btn">게시판</button>
+            <ul class="header-list-flex">
+                <li class="header-list-hover"><a href="">호텔</a></li>
+                <li class="header-list-hover"><a href="">상품</a></li>
+                <li class="header-list-hover">
+                    <div class="header-list-dropdown">
+                        <a href="" class="header-list-dropbtn">게시판</a>
+                        <div class="header-list-hover header-list-dropdown-content">
+                            <a href="#">리뷰</a>
+                            <a href="#">자유</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
             <div class="header-search"> 
                 <input class="header-search-bar" type="text" required placeholder="Q 어디로 놀러가세요?" >
                 <button class="btn bg-navy header-bg-btn">검색</button>
@@ -117,12 +127,37 @@ a {
     text-decoration: none;
 }
 
+a:visited {
+    color : #01083A;
+}
+
+li {
+    list-style-type: none;
+}
+
 input, textarea {
     border: none;
     outline: none;
 }
 
-/* 네비게이션 메뉴 */
+.btn {
+    border: none;
+    text-decoration: none;
+    cursor: pointer;
+    text-align: center; 
+}
+
+.bg-navy {
+    background-color: #01083A;
+    color: #ffff;
+}
+
+.bg-clear {
+    background-color: transparent;
+    color: #01083A;
+}
+
+/* 네비게이션 메뉴 -> 현재 생략 */
 .navi {
     position: relative;
     width: 200px;
@@ -130,7 +165,10 @@ input, textarea {
     background-color: #01083A;
 }
 
-/* 헤더해더 */
+/* ************************************ */
+
+/* 헤더헤더 */
+
 header {
     display: grid;
     grid-template-rows: 100px 50px;
@@ -143,43 +181,45 @@ header {
     min-width: 600px;
     gap: 15px;
 }
+
+/* 헤더 1 -> 로고 , 로그인 버튼 */
 .header-title {
     display: grid;
     grid-template-columns: 270px 1fr 270px;
     align-items: center;
     justify-items: center;
 }
+
 .header-title > :first-child {
     justify-self: start;
     margin-left: 50px;
 }
+
 .header-title-button {
     display: flex;
     gap: 25px;
 }
 
-.header-list {
-    display: flex;
-    align-items: center;
-    /* margin-left: 20px; */
-    padding: 25px;
-    gap: 20px;
-}
 .header-list > :last-child {
     margin-left: auto;
 }
+
 .header-logo-img {
     width: 110px;
     height: 110px;
     /* min-width: 100px; */
 }
+
 .header-title-img {
     width: 450px;
     /* height: 110px; */
 }
+
+/* 헤더 로그인 버튼 */
 .header-btn {
     font-size: 18px;
 }
+
 .header-bg-btn {
     font-size: 18px;
     width: 70px;
@@ -189,12 +229,95 @@ header {
     align-items: center;
     justify-content: center;
 }
+
+
+/* 헤더 2 -> 호텔, 상품, 게시판, 검색바 */
+.header-list {
+    display: flex;
+    align-items: center;
+    /* margin-left: 20px; */
+    padding: 25px;
+    gap: 20px;
+}
+
+/* 호텔, 상품, 게시판 hover */
+.header-list-flex {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+}
+
+/* .header-list-hover {
+    margin-top: 5px;
+} */
+
+.header-list-hover a {
+    color: #01083A;
+    font-size: 23px;
+    font-weight: 400;
+    padding: 5px 10px;
+    /**/
+    position: relative;
+    display: block;
+    text-decoration: none;
+    text-transform: uppercase;
+    text-align: center;
+    margin-top: 5px;
+}
+
+.header-list-hover a:before, .header-list-hover a:after {
+    content: "";
+    transition: 0.3s all ease;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    position: absolute;
+    width: 0;
+    height: 2px;
+}
+
+.header-list-hover a:before {
+    right: 0;
+    top: 0;
+}
+
+.header-list-hover a:after {
+    left: 0;
+    bottom: 0;
+}
+
+.header-list-hover a:hover:before, .header-list-hover a:hover:after {
+    width: 100%;
+    background: #01083A;
+}
+
+.header-list-dropdown{
+    position : relative;
+    display : inline-block;
+}
+
+.header-list-dropdown-content{
+    display : none;
+    position : absolute;
+    z-index : 1; /*다른 요소들보다 앞에 배치*/
+}
+
+.header-list-dropdown-content a{
+    display : block;
+    font-size: 18px;
+}
+
+.header-list-dropdown:hover .header-list-dropdown-content {
+    display: block;
+}
+
+/* 헤더 검색바 */
 .header-search {
     display: flex;
     align-items: center;
     /* margin-right: 10px; */
     gap: 20px;
 }
+
 .header-search-bar {
     border-radius: 50px;
     width: 330px;
@@ -205,39 +328,7 @@ header {
     padding-left: 20px;
 }
 
-.header-list > button {
-    background-color:transparent;
-    font-size: 25px;
-    color: #01083A;  
-    transition: color .7s ease-in-out, box-shadow .7s ease-in-out;
-}
-
-.header-list >  button:hover {
-    box-shadow: inset 100px 0 0 0 #01083A;
-    color: #fff;
-}
-
-/* 여러곳에 사용될 버튼 css */
-.btn {
-    border: none;
-    text-decoration: none;
-    cursor: pointer;
-    text-align: center; 
-}
-
-/* .btn:hover {
-    font-weight: 700;
-} */
-
-.bg-navy {
-    background-color: #01083A;
-    color: #ffff;
-}
-
-.bg-clear {
-    background-color: transparent;
-    color: #01083A;
-}
+/* ************************************ */
 
 /* 메인메인 */
 .main {
@@ -246,6 +337,8 @@ header {
     max-width: 1300px;
     min-width: 600px;
 }
+
+/* ************************************ */
 
 /* 푸터푸터 */
 footer {
@@ -260,6 +353,8 @@ footer {
     margin-top: 100px;
     gap: 30px;
 }
+
+/* 푸터 사업자번호 */
 .footer-inner {
     padding-top: 40px;
     /* display: grid;
@@ -269,16 +364,20 @@ footer {
     align-items: center;
     gap: 30px;
 }
+
 .footer-inner > :nth-child(1) {
     justify-self: end;
 }
+
 .footer-inner > :nth-child(2) {
     justify-self: start;
 }
+
 .footer-logo {
     height: 140px;
     width: 140px;
 }
+
 .footer-text {
     color: #ffff;
     font-size: 15px;
@@ -286,13 +385,16 @@ footer {
     white-space: nowrap;
     line-height: 1.7rem;
 }
+
 .footer-bottom {
     margin: 0 auto;
     padding: 10px;
 }
+
 .hr {
     width: 95vw;
 }
+
 .copy-right {
     font-size: 13px;
     color: #fff;
