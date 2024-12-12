20ab4964490d99894561d249eeb431fb402332cc
LeeGyeongjinny <yikj555@gmail.com>
2024-12-12 18:13:52 +0900

    241212 이경진 오늘 작업

--- IgoUgo/resources/js/store/store.js
+++ IgoUgo/resources/js/store/store.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 import user from './modules/user';
+import auth from './modules/auth';
 import board from './modules/board';
 
 export default createStore({
     modules: {
         user,
+        auth,
         board,
     }
 })
