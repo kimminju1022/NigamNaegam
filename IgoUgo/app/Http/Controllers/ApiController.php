5de54e9515e8a33559cef68f0d0e4ed88fad65d4
YJS2000 <yjs03744959@gmail.com>
2024-12-16 14:57:20 +0900

    241216 윤종승 보드디포트 사진변경 밑 리스트페이지 css 변경

--- IgoUgo/app/Http/Controllers/ApiController.php
+++ IgoUgo/app/Http/Controllers/ApiController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
     public function hotelProducts()
     {
-        $hotels = Http::get('http://apis.data.go.kr/B551011/KorService1/searchStay1?serviceKey='. env('API_KEY') .'&numOfRows=16&pageNo=1&MobileOS=ETC&MobileApp=IgoUgo&_type=json');
+        $url = 'http://apis.data.go.kr/B551011/KorService1/searchStay1';
+        $serviceKey = env('API_KEY');
+        // LOG::debug($serviceKey);
+
+        $hotels = HTTP::get($url, [
+            'serviceKey' => $serviceKey,
+            'numOfRows' => 20,
+            'pageNo' => 1,
+            'MobileOS' => 'ETC',
+            'MobileApp' => 'IgoUgo',
+            '_type' => 'json',
+        ]);
+
+        $resultCode = $hotels->header('resultCode');
+
+        Log::debug($serviceKey);
+
+        if($hotels->failed() && $resultCode !== '0000') {
+            throw new \Exception('API 받아오기 실패'. $hotels->status());
+        }
 
         return response()->json($hotels->json());
     }
--- IgoUgo/resources/views/components/board/HotelListComponent.vue
+++ IgoUgo/resources/views/components/board/HotelListComponent.vue
@@ -6,35 +6,55 @@
             <h2 class="map-box-title" :class="{'dis-none': flg}">지도로 보기</h2>
         </div>
         <div class="left-small-container">
-            <h2 class="cat-box-title">카테고리</h2>
+            <h2 class="cat-box-title">시설</h2>
             <div class="cat-list" :class="{'cat-list-change':flg}">
                 <div class="list-item" :class="{'cat-list-item-change':flg}">
-                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="food" value="관광지">
-                    <label for="food">관광지</label>
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="accomcountlodging" value="0">
+                    <label for="accomcountlodging">수용가능인원</label>
                 </div>
                 <div class="list-item" :class="{'cat-list-item-change':flg}">
-                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="activity" value="문화시설">
-                    <label for="activity">문화시설</label>
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="checkintime" value="1">
+                    <label for="checkintime">입실시간</label>
                 </div>
                 <div class="list-item" :class="{'cat-list-item-change':flg}">
-                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="shopping" value="이벤트">
-                    <label for="shopping">이벤트</label>
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="checkouttime" value="2">
+                    <label for="checkouttime">퇴실시간</label>
                 </div>
                 <div class="list-item" :class="{'cat-list-item-change':flg}">
-                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="healing" value="레포츠">
-                    <label for="healing">레포츠</label>
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="chkcooking" value="3">
+                    <label for="chkcooking">객실 내 취사여부</label>
                 </div>
                 <div class="list-item" :class="{'cat-list-item-change':flg}">
-                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="shopping" value="쇼핑">
-                    <label for="shopping">쇼핑</label>
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="foodplace" value="4">
+                    <label for="foodplace">식음료장</label>
                 </div>
                 <div class="list-item" :class="{'cat-list-item-change':flg}">
-                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="healing" value="음식점">
-                    <label for="healing">음식점</label>
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="pickup" value="5">
+                    <label for="pickup">픽업서비스</label>
+                </div>
+                <div class="list-item" :class="{'cat-list-item-change':flg}">
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="parkinglodging" value="음식점">
+                    <label for="parkinglodging">주차시설</label>
+                </div>
+                <div class="list-item" :class="{'cat-list-item-change':flg}">
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="parkinglodging" value="음식점">
+                    <label for="parkinglodging">주차시설</label>
+                </div>
+                <div class="list-item" :class="{'cat-list-item-change':flg}">
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="parkinglodging" value="음식점">
+                    <label for="parkinglodging">주차시설</label>
+                </div>
+                <div class="list-item" :class="{'cat-list-item-change':flg}">
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="parkinglodging" value="음식점">
+                    <label for="parkinglodging">주차시설</label>
+                </div>
+                <div class="list-item" :class="{'cat-list-item-change':flg}">
+                    <input class="cat-input" :class="{'cat-input-change':flg}" type="checkbox" name="category" id="parkinglodging" value="음식점">
+                    <label for="parkinglodging">주차시설</label>
                 </div>
             </div>
         </div>
-        <div class="left-small-container">
+        <!-- <div class="left-small-container">
             <h2 class="pri-box-title">가격</h2>
             <div class="pri-box" :class="{'pri-box-change':flg}">
                 <input type="number" class="pri-input" max="300000000">
@@ -46,7 +66,7 @@
         </div>
         <div class="button-position">
             <button class="btn bg-navy button-wide">적용</button>
-        </div>
+        </div> -->
     </div>
 
     <div>
@@ -164,7 +184,7 @@ window.addEventListener('resize', flgSetup);
 
 // 이미지 없을때 나오는 사진
 const setDefaultImage = (event) => {
-  event.target.src = '/logo_gam.png';
+  event.target.src = '/default/board_default.png';
 }
 
 // API 받아온거 axios로 불러오기기
@@ -197,6 +217,7 @@ onMounted(async() => {
     border-radius: 10px;
     margin: 1rem 0;
     padding: 20px;
+    min-width: 200px;
 }
 .right-small-container {
     /* border: 1px solid #01083A; */
@@ -235,6 +256,7 @@ onMounted(async() => {
     /* justify-content: space-around; */
     gap: 20px;
     margin-top: 10px;
+    height: 545px;
 }
 .cat-input {
     width: 20px;
@@ -322,17 +344,18 @@ onMounted(async() => {
     border-radius: 10px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     padding: 20px;
 }
 .img-card {
-    min-width: 100px;
-    min-height: 100px;
-    max-width: 196px;
-    max-height: 196px;
+    /* min-width: 100px;
+    min-height: 100px; */
+    width: 185px;
+    height: 185px;
     object-fit: cover;
     background-repeat: no-repeat;
+    border-radius: 5px;
 }
 .lodingmsg {
     margin-top: 50px;
