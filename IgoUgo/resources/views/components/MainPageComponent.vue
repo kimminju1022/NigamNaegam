6342b0489c7083999bfc92ccd8bfe1ddf165d151
jji-min <6wnla9@naver.com>
2024-12-17 18:13:57 +0900

    241217 김지민 메인페이지 슬라이드 작업중

--- IgoUgo/resources/views/components/MainPageComponent.vue
+++ IgoUgo/resources/views/components/MainPageComponent.vue
@@ -1,7 +1,85 @@
 <template>
 <div class="main-page-container">
-    <div>
-        <img src="default/main_banner.png" class="main-banner-img">
+    <div class="main-banner">
+        <!-- <img src="default/main_banner.png" class="main-banner-img"> -->
+        <swiper
+            :pagination="{
+                type: 'fraction',
+            }"
+            :scrollbar="true"
+            :autoplay="{
+                delay: 360000,
+                disableOnInteraction: false,
+            }"
+            :centeredSlides="true"
+            :slidesPerView="1"
+            :spaceBetween="30"
+            :loop="true"
+            :navigation="{
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next',
+            }"
+            :modules="modules"
+            class="mySwiper"
+        >
+            <swiper-slide>
+                <div class="banner-slide banner-slide1"></div>
+                <div class="banner-bottom-white"></div>
+                <div :class="flg ? 'banner-content' : 'banner-content-flex'">
+                    <img class="banner-content-img" :class="{'ch-size':flg}" src="img_main/homi.png">
+                    <div class="banner-content-right">
+                        <div>새해는 일출과 함께</div>
+                        <div>호미곶</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="banner-slide banner-slide2"></div>
+                <div class="banner-bottom-white"></div>
+                <div :class="flg ? 'banner-content' : 'banner-content-flex'">
+                    <img class="banner-content-img" :class="{'ch-size':flg}" src="img_main/chris.jpg">
+                    <div class="banner-content-right">
+                        <div>메리 크리스마스~</div>
+                        <div>더현대</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="banner-slide banner-slide3"></div>
+                <div class="banner-bottom-white"></div>
+                <div :class="flg ? 'banner-content' : 'banner-content-flex'">
+                    <img class="banner-content-img" :class="{'ch-size':flg}" src="img_main/duck.png">
+                    <div class="banner-content-right">
+                        <div>나랑~ 설산에~ 등산~ 갈래~?</div>
+                        <div>한국의 대표 설산으로 꼽히는 덕유산</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="banner-slide banner-slide4"></div>
+                <div class="banner-bottom-white"></div>
+                <div :class="flg ? 'banner-content' : 'banner-content-flex'">
+                    <img class="banner-content-img" :class="{'ch-size':flg}" src="img_main/souna.png">
+                    <div class="banner-content-right">
+                        <div>자연을 만끽하며 뜨끈한 물에~</div>
+                        <div>사우나</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="banner-slide banner-slide5"></div>
+                <div class="banner-bottom-white"></div>
+                <div :class="flg ? 'banner-content' : 'banner-content-flex'">
+                    <img class="banner-content-img" :class="{'ch-size':flg}" src="img_main/haedong.png">
+                    <div class="banner-content-right">
+                        <div>숨겨진 일출 맛집</div>
+                        <div>부산 해동용궁사</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </swiper>
     </div>
     
     <div class="main-together-container">
@@ -43,7 +121,7 @@
         <!-- 관광특구와 같은 홍보글 넣을것, 일단 이미지로 넣었음 -->
         <div class="main-ppl-container">
             <img src="default/profile_default.png" class="main-ppl-ex">
-            <img src="default/board_default.png" class="main-ppl-ex">
+            <img src="default/profile_default.png" class="main-ppl-ex">
             <img src="default/profile_default.png" class="main-ppl-ex">
         </div>
     </div>
@@ -113,6 +191,29 @@
 </template>
 
 <script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { onBeforeMount, ref } from 'vue';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+
+// Import required modules
+import { Pagination, Navigation, Scrollbar, Autoplay } from 'swiper/modules';
+
+// Register Swiper modules
+const modules = [Pagination, Navigation, Scrollbar, Autoplay];
+
+const flg = ref(false);
+const flgSetup = () => {
+    flg.value = window.innerWidth >= 1000 ? false : true;
+}
+onBeforeMount(() => {
+    flgSetup();
+});
+window.addEventListener('resize', flgSetup);
 </script>
 
 <style scoped>
@@ -124,8 +225,78 @@
 }
 
 /* 베너 */
-.main-banner-img {
+.main-banner {
+    height: 500px;
+}
+/* .main-banner-img {
     width: 100%;
+} */
+/* ***** swiper CSS ***** */
+/* swiper 전체 */
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+/* swiper 내부 */
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+/* img 없으니까 일단 뺌 */
+/* .swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+} */
+/* slide */
+.banner-slide {
+    width: 100%;
+    height: 75%;
+}
+.banner-bottom-white {
+    background-color: #fff;
+    width: 100%;
+    height: 25%;
+}
+.banner-content {
+    width: 80%;
+    /* display: flex; */
+    position: absolute;
+    gap: 20px;
+}
+.banner-content-img {
+    width: 400px;
+    height: 400px;
+    border-radius: 30px;
+}
+.banner-content-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+.banner-slide1 {
+    background-color: #faba90;
+}
+.banner-slide2 {
+    background-color: #025B00;
+}
+.banner-slide3 {
+    background-color: #99bff2;
+}
+.banner-slide4 {
+    background-color: #d7bda6;
+}
+.banner-slide5 {
+    background-color: lavender;
+}
+.swiper-button-prev, .swiper-button-next {
+  color: #01083a;
 }
 
 /* 같이할래 부분 */
@@ -184,5 +355,18 @@
     border: 1px solid #01083a;
     border-radius: 10px;
     min-width: 200px;
+}
+
+/* ---------- 반응형 ---------- */
+/* 베너 관련 */
+.ch-size {
+    width: 250px;
+    height: 350px;
+}
+.banner-content-flex {
+    width: 80%;
+    display: flex;
+    position: absolute;
+    gap: 20px;
 }
 </style>
