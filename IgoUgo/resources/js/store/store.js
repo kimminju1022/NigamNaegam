e7a40f4cc82d65f3f8bc2bf09a492496c1d5f8e8
kimminju1022 <nanum112@naver.com>
2024-12-19 15:31:54 +0900

    241219김민주question등록

--- IgoUgo/resources/js/store/store.js
+++ IgoUgo/resources/js/store/store.js
@@ -2,11 +2,13 @@ import { createStore } from 'vuex';
 import user from './modules/user';
 import auth from './modules/auth';
 import board from './modules/board';
+import question from './modules/question';
 
 export default createStore({
     modules: {
         user,
         auth,
         board,
+        question,
     }
 })
