1b290e1d148168caa721a0816a4e51b8e975890a
jji-min <6wnla9@naver.com>
2025-01-16 15:06:03 +0900

    250116 김지민 notice factory 수정

--- IgoUgo/database/factories/NoticeFactory.php
+++ IgoUgo/database/factories/NoticeFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,15 +18,31 @@ class NoticeFactory extends Factory
      */
     public function definition()
     {
-        $user = User::select('user_id')->where('manager_flg', '1')->inRandomOrder()->first();
-        $date = $this->faker->dateTimeBetween('-1 year');
+        $user = User::select('user_id', 'created_at')->where('manager_flg', '1')->inRandomOrder()->first();
+        $notice_flg = rand(0,1);
+        
+        $date = $this->faker->dateTimeBetween($user->created_at, now());
+
+        $flg_date = Carbon::now()->subMonths(6);
+
+        if($notice_flg === 1) {
+            if(Carbon::parse($date)->lt($flg_date)) {
+                $update = Carbon::parse($date)->addMonths(6);
+            } else {
+                $update = $date;
+            }
+        } else if($notice_flg === 0) {
+            $update = $date;
+        }
 
         return [
             'user_id' => $user->user_id,
+            'notice_flg' => $notice_flg,
             'notice_title' => $this->faker->realText(rand(10, 100)),
             'notice_content' => $this->faker->realText(rand(10, 2000)),
             'created_at' => $date,
-            'updated_at' => $date,
+            'updated_at' => $update,
+            'deleted_at' => $update === $date ? null : $update,
         ];
     }
 }
