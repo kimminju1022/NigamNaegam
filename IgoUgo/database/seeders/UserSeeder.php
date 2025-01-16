f9c386cd0fbffdb1e60f5b45075a26f9f051e78e
LeeGyeongjinny <yikj555@gmail.com>
2025-01-16 14:44:23 +0900

    250116 이경진 UserSeeder 수정

--- IgoUgo/database/seeders/UserSeeder.php
+++ IgoUgo/database/seeders/UserSeeder.php
@@ -36,14 +36,23 @@ class UserSeeder extends Seeder
     public function run()
     {
 
-        $date = $this->faker->dateTimeBetween('-1 year');
+        $date = '2024-01-01 00:00:00';
         
         $data = [
-            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '이경진', 'user_nickname' => '이경진',  'user_phone' => '01011111111', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => now(), 'created_at' => $date, 'updated_at' => $date],
-            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test2@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '김지민', 'user_nickname' => '김지민', 'user_phone' => '01022222222', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => now(), 'created_at' => $date, 'updated_at' => $date],
-            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test3@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '윤종승', 'user_nickname' => '윤종승', 'user_phone' => '01033333333', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => now(), 'created_at' => $date, 'updated_at' => $date],
-            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test4@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '김민주', 'user_nickname' => '김민주', 'user_phone' => '01044444444', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => now(), 'created_at' => $date, 'updated_at' => $date],
+            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '이경진', 'user_nickname' => '이경진',  'user_phone' => '01011111111', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
+            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test2@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '김지민', 'user_nickname' => '김지민', 'user_phone' => '01022222222', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
+            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test3@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '윤종승', 'user_nickname' => '윤종승', 'user_phone' => '01033333333', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
+            ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test4@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '김민주', 'user_nickname' => '김민주', 'user_phone' => '01044444444', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
         ];
+
+        // $date = $this->faker->dateTimeBetween('-1 year');
+        
+        // $data = [
+        //     ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '이경진', 'user_nickname' => '이경진',  'user_phone' => '01011111111', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
+        //     ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test2@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '김지민', 'user_nickname' => '김지민', 'user_phone' => '01022222222', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
+        //     ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test3@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '윤종승', 'user_nickname' => '윤종승', 'user_phone' => '01033333333', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
+        //     ['manager_flg'=> '1', 'user_flg' => '0', 'user_email' => 'test4@test.com', 'user_password' => Hash::make('qwer1234!'), 'user_name' => '김민주', 'user_nickname' => '김민주', 'user_phone' => '01044444444', 'user_profile' => '/default/profile_default.png', 'email_verified_at' => $date, 'created_at' => $date, 'updated_at' => $date],
+        // ];
 
         foreach($data as $item) {
             User::create($item);
