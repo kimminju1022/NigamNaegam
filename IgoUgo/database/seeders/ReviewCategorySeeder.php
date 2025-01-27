65ce817fbcfdbbf9993f79f89d636131d9053c64
jji-min <6wnla9@naver.com>
2025-01-27 17:09:10 +0900

    250127 김지민 모델 정리

--- IgoUgo/database/seeders/ReviewCategorySeeder.php
+++ IgoUgo/database/seeders/ReviewCategorySeeder.php
@@ -23,13 +23,12 @@ class ReviewCategorySeeder extends Seeder
             // ,['rc_type' => '3', 'rc_name' => '문화']
             // ,['rc_type' => '4', 'rc_name' => '레포츠']
             // ,['rc_type' => '5', 'rc_name' => '쇼핑']
-            ['rc_code' => '0', 'rc_name' => '숙박']
-            ,['rc_code' => '1', 'rc_name' => '관광']
-            ,['rc_code' => '2', 'rc_name' => '문화시설']
-            ,['rc_code' => '3', 'rc_name' => '레포츠']
-            ,['rc_code' => '4', 'rc_name' => '쇼핑']
-            ,['rc_code' => '5', 'rc_name' => '음식점']
-            ,['rc_code' => '6', 'rc_name' => '루트']
+            ['rc_code' => '12', 'rc_name' => '관광지']
+            ,['rc_code' => '14', 'rc_name' => '문화시설']
+            ,['rc_code' => '28', 'rc_name' => '레포츠']
+            ,['rc_code' => '32', 'rc_name' => '호텔']
+            ,['rc_code' => '38', 'rc_name' => '쇼핑']
+            ,['rc_code' => '39', 'rc_name' => '음식점']
         ]);
     }
 }
