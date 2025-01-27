81b48079807e93bd86c14faef244ee60323ec28d
LeeGyeongjinny <yikj555@gmail.com>
2025-01-27 16:28:19 +0900

    250127 이경진 migration 수정중

--- IgoUgo/database/migrations/2024_12_20_121638_create_hotel_categories_table.php
+++ IgoUgo/database/migrations/2024_12_20_121638_create_hotel_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('hotel_categories', function (Blueprint $table) {
             $table->id('hc_id');
             // $table->integer('hc_code')->unique(); // 얘는 왜 또 integer?
-            $table->char('hc_code', 1)->unique();
+            $table->char('hc_code', 1)->unique()->comment('0: 수영장, 1: 바베큐장, 2: 캠프파이어, 3: 뷰티시설, 4: 피트니스센터, 5: 픽업서비스');
             $table->string('hc_name', 10);
         });
     }
